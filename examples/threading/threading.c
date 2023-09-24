543377e37bbcf0f80bb11967f18aeb2a0fdd12ce
Isa <isa080199@gmail.com>
2023-09-23 20:00:13 -0700

    threads

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -13,21 +15,61 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    int res = usleep(thread_func_args->wait_to_obtain_ms);
+
+    if (res != 0)
+        return thread_func_args;
+
+    res = pthread_mutex_lock(thread_func_args->mutex);
+
+    if (res != 0)
+        return thread_func_args;
+
+    res = usleep(thread_func_args->wait_to_release_ms);
+
+    if (res != 0)
+        return thread_func_args;
+
+    res = pthread_mutex_unlock(thread_func_args->mutex);
+
+    if (res != 0)
+        return thread_func_args;
+
+
+    thread_func_args->thread_complete_success = true;
+
+    return thread_func_args;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
+    
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+    * using threadfunc() as entry point.
+    *
+    * return true if successful.
+    *
+    * See implementation details in threading.h file comment block
+    */
+
+    struct thread_data *data = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    data->id = thread;
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+
+    int res = pthread_create(data->id, NULL, &threadfunc, data);
+
+    if (res != 0)
+        return false;
+
+    return true;
+
 }
 
