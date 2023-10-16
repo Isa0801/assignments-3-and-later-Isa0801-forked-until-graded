a92cd04d41bc2f5e300f9d95341c6b18b012ecef
Isa <isa080199@gmail.com>
2023-10-17 01:58:09 +0200

    adding fix

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -116,7 +116,7 @@ void before_exit()
 	
 }
 
-void hande_signal()
+void handel_signal()
 {
     syslog(LOG_USER, "Caught signal, exiting");
     exit(0);
@@ -124,7 +124,7 @@ void hande_signal()
 
 void* handle_connection_threaded(void*);
 
-void* handle_timerstamp_threaded(void*);
+void handle_timerstamp_signal();
 
 
 int main(int argc, char **argv)
@@ -146,8 +146,9 @@ int main(int argc, char **argv)
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	addr = (struct sockaddr_in){.sin_family = AF_INET, .sin_port = htons(9000), .sin_addr = (struct in_addr){.s_addr = INADDR_ANY}};
 
-	signal(SIGTERM, hande_signal);
-	signal(SIGINT, hande_signal);
+	signal(SIGTERM, handel_signal);
+	signal(SIGINT, handel_signal);
+	signal(SIGALRM, handle_timerstamp_signal);
 	
 	int option = 1;
 	
@@ -188,34 +189,21 @@ int main(int argc, char **argv)
 	}
 
 
-	struct entry_thread *p_thread = malloc(sizeof(struct entry_thread));
+	alarm(10);
 
-	int created = pthread_create(&p_thread->thread_fd, NULL, &handle_timerstamp_threaded, NULL);
-
-	if (created == -1)
-	{
-		printf("could not create thread\n");
-	}
-
-	if (SLIST_EMPTY(&list_head))
-		{
-			SLIST_INSERT_HEAD(&list_head, p_thread, entries);
-		} else if (SLIST_EMPTY(&list_head))
-		{
-			SLIST_INSERT_AFTER(SLIST_FIRST(&list_head), p_thread, entries);
-	}
 
 	while (1)
 	{
-
+		printf("waiting for new connection\n");
 		client_fd = accept(socket_fd, (struct sockaddr *) &addr, &addr_size);
+		printf("accepted %d\n", client_fd);
 
 		if (client_fd == -1)
 		{
 			exit(-1);
 		}
 
-		p_thread = malloc(sizeof(struct entry_thread));
+		struct entry_thread *p_thread  = malloc(sizeof(struct entry_thread));
 
 		inet_ntop(AF_INET, &addr.sin_addr, client_addr, INET_ADDRSTRLEN);
 		syslog(LOG_USER, "Accepted connection from %s", client_addr);
@@ -245,33 +233,24 @@ int main(int argc, char **argv)
 }
 
 
-void* handle_timerstamp_threaded(void *arg)
+void handle_timerstamp_signal()
 {
 
-	time_t old_time, current_time;
+	time_t current_time;
 	struct tm *tm_time;
 	char buffer[50];
 
+
 	time(&current_time);
-	time(&old_time);
 
-	while (running_timestamp)
-	{
-		time(&current_time);
+	tm_time = localtime(&current_time);
+	int bytes = strftime(buffer, 50, "timestamp:%c\n", tm_time);
 
-		if ((current_time - old_time) >= 10)
-		{
-			old_time = current_time;
-			tm_time = localtime(&current_time);
-			int bytes = strftime(buffer, 50, "timestamp:%c\n", tm_time);
-			
-	
-			pthread_mutex_lock(&global_mutex);
-			write_to_aesdsockdata(buffer, bytes);
-			pthread_mutex_unlock(&global_mutex);
+	pthread_mutex_lock(&global_mutex);
+	write_to_aesdsockdata(buffer, bytes);
+	pthread_mutex_unlock(&global_mutex);
 
-		}
-	}
+	alarm(10);
 
 }
 
@@ -281,13 +260,16 @@ void* handle_connection_threaded(void *arg)
 	int res;
 	int client_fd = *(int*)arg;
 	free(arg);
-	int data_buffer_size = sizeof(char) * 512;
+	int data_buffer_size = sizeof(char) * 1024;
 	char *data_buffer = malloc(sizeof(char) * data_buffer_size);
 
-	while((res = read(client_fd, &data_buffer[counter], 1)) == 1)
+	printf("starting to read from %d\n", client_fd);
+	while((res = recv(client_fd, &data_buffer[counter], 1, 0)) == 1)
 	{
+		printf("read: %c\n", data_buffer[counter]);
 		if (res == -1)
 		{
+			printf("err%d\n", client_fd);
 			pthread_exit(NULL);
 		}
 
@@ -316,6 +298,7 @@ void* handle_connection_threaded(void *arg)
 	*(newline + 1) = '\0';
 
 	pthread_mutex_lock(&global_mutex);
+	printf("read%s\n", data_buffer);
 	write_to_aesdsockdata(data_buffer, (newline - data_buffer) + 1);
 	pthread_mutex_unlock(&global_mutex);
 
@@ -329,8 +312,12 @@ void* handle_connection_threaded(void *arg)
 	pthread_mutex_lock(&global_mutex);
 	write(client_fd, data_buffer, data_to_send);
 	pthread_mutex_unlock(&global_mutex);
-	
-	close(client_fd);
+
+	printf("closing off%d\n", client_fd);
+
+	if (client_fd != -1)
+		close(client_fd);
+
 
 	free(data_buffer);
 }
